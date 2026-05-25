cdf59d8e7ee7924f0020ac497badff4f32d32fd8
lolacsAcademy <lola.rouquet@cyber.dlh.lu>
2026-05-25 05:16:46 -0400

    Fix whois csv format

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^Registrant |^Admin |^Tech /{if($1~/Street/) print $1","$2" "; else if($1~/Ext/) print $1","$2; else print $1","$2}' > $1.csv
+whois $1 | awk -F': ' '/^Registrant |^Admin |^Tech /{if($1~/Street/) print $1", "$2; else if($1~/Ext/) print $1","$2; else print $1", "$2}' > $1.csv
