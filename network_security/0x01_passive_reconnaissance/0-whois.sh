9d5e022ee5913ad477cc916e3438eaa401a4a11f
lolacsAcademy <lola.rouquet@cyber.dlh.lu>
2026-05-26 02:05:20 -0400

    Remove generated files

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/^Registrant |^Admin |^Tech /{if($1~/Street/) print $1", "$2" "; else if($1~/Ext/) print $1","$2; else if($2=="") print $1","; else print $1", "$2}' > $1.csv
+whois $1 | awk -F': ' '/^Registrant |^Admin |^Tech /{v=$2; if($1~/Street/) v=v" "; print $1", "v}' > $1.csv
